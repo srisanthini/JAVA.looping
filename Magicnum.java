63b03b1784d457a073e2306f0aa246ae9084fe9c
srisanthini <srisanthinisivanandham@gmail.com>
2026-01-06 14:08:53 +0530

    Add files via upload

---
+++ Magicnum.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+public class Magicnum {   
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        int sum = 0;
+
+        while (num > 9) {
+            sum = 0;
+            while (num != 0) {
+                int digit = num % 10;
+                sum += digit;
+                num /= 10;
+            }
+            num = sum;
+        }
+
+        if (num == 1) {
+            System.out.println("The number is a Magic Number");
+        } else {
+            System.out.println("The number is NOT a Magic Number");
+        }
+        sc.close();
+    }
+}
---
+++ Neonnum.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+public class Neonnum {   
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        int square = num * num;
+        int sumOfDigits = 0;
+
+        while (square != 0) {
+            int digit = square % 10;
+            sumOfDigits += digit;
+            square /= 10;
+        }
+
+        if (sumOfDigits == num) {
+            System.out.println(num + " is a Neon Number");
+        } else {
+            System.out.println(num + " is NOT a Neon Number");
+        }
+        sc.close();
+    }
+}
